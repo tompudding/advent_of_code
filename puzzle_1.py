acef9a0e3a5271537068d7afa7554a97cc411370
Tom Gooding <tom.pudding@gmail.com>
2021-12-02 08:14:22 +0000

    black

--- puzzle_1.py
+++ puzzle_1.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
+
 def delta(filename, n):
 
     count = 0
-    with open(sys.argv[1],'r') as file:
-        buffer = [int(file.readline().strip(),10) for i in range(n)]
+    with open(sys.argv[1], "r") as file:
+        buffer = [int(file.readline().strip(), 10) for i in range(n)]
 
         for line in file:
             try:
@@ -19,6 +20,7 @@ def delta(filename, n):
             buffer.pop(0)
             buffer.append(n)
     return count
+
 
 print(delta(sys.argv[1], 1))
 print(delta(sys.argv[1], 3))
