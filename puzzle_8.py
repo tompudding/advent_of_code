cef4fbfbb5861854bd79eddc045dc3c3c30f4d06
Tom Gooding <tom.pudding@gmail.com>
2021-12-09 19:47:52 +0000

    Be a bit more pythonic

--- puzzle_8.py
+++ puzzle_8.py
@@ -1,5 +1,5 @@
 import sys
-
+import collections
 
 digits_to_segments = {
     "0": "abcefg",
@@ -15,27 +15,12 @@ digits_to_segments = {
 }
 
 
-def get_counts(words):
-    counts = {}
-
-    for segment in "".join(words):
-        try:
-            counts[segment] += 1
-        except KeyError:
-            counts[segment] = 1
-    return counts
-
-
 def get_score(word, segment_counts):
-    score = 0
-    for segment in word:
-        score += segment_counts[segment]
-
-    return score
+    return sum((segment_counts[segment] for segment in word))
 
 
 canonical_scores = {}
-canonical_counts = get_counts(digits_to_segments.values())
+canonical_counts = collections.Counter("".join(digits_to_segments.values()))
 canonical_scores = {get_score(word, canonical_counts): digit for digit, word in digits_to_segments.items()}
 
 assert len(canonical_scores) == 10
@@ -44,7 +29,7 @@ assert len(canonical_scores) == 10
 def process_line(line):
     patterns, output = (part.split() for part in line.split("|"))
 
-    segment_counts = get_counts(patterns)
+    segment_counts = collections.Counter("".join(patterns))
 
     words_to_digit = {}
 
@@ -63,8 +48,7 @@ with open(sys.argv[1], "r") as file:
     for line in file:
         patterns, output = (part.split() for part in line.split("|"))
         for word in output:
-            if len(word) in [2, 4, 3, 7]:
-                count += 1
+            count += 1 if len(word) in {2, 4, 3, 7} else 0
 
 print(count)
 
