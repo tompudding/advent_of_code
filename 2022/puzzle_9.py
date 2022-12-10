79aef6a64fd4b512319be885b8b8dc68b214232a
Tom Gooding <tom@gooding.xyz>
2022-12-10 03:20:49 +0000

    Update for part 2

--- 2022/puzzle_9.py
+++ 2022/puzzle_9.py
@@ -1,34 +1,53 @@
 import sys
 
-head = [0,0]
-tail = [0,0]
+def add(a, b):
+    return (a[0]+b[0], a[1]+b[1])
 
-vectors = {'R' : (1,0),
-           'U' : (0,1),
-           'L' : (-1,0),
-           'D' : (0,-1)}
+class Chain:
+    vectors = {'R' : (1,0),
+               'U' : (0,1),
+               'L' : (-1,0),
+               'D' : (0,-1)}
 
-def step_tail(head, tail):
-    diff = (head[0] - tail[0], head[1] - tail[1])
-    if abs(diff[0]) <= 1 and abs(diff[1]) <= 1:
-        return tail
-    diff = [x/abs(x) if x else 0 for x in diff]
-    tail[0] += diff[0]
-    tail[1] += diff[1]
-    return tail
+    def __init__(self, num_knots):
+        self.rope = [(0,0) for i in range(num_knots)]
+        self.tails = set()
 
-tails = set()
+    def step(self, cmd):
+        vector = self.vectors[cmd]
 
+        self.rope[0] = add(self.rope[0], vector)
+
+        for i in range(1, len(self.rope)):
+            self.rope[i] = self.update(self.rope[i-1], self.rope[i])
+
+        self.tails.add(tuple(self.rope[-1]))
+
+    @staticmethod
+    def update(head, tail):
+        diff = (head[0] - tail[0], head[1] - tail[1])
+        if abs(diff[0]) <= 1 and abs(diff[1]) <= 1:
+            return tail
+        diff = [x/abs(x) if x else 0 for x in diff]
+        return add(tail, diff)
+
+commands = []
 with open(sys.argv[1],'r') as file:
     for line in file:
         cmd, num = line.strip().split()
-        vector = vectors[cmd]
         num = int(num)
-        for i in range(num):
-            head[0] += vector[0]
-            head[1] += vector[1]
-            tail = step_tail(head, tail)
-            tails.add(tuple(tail))
-            print(head, tail)
+        commands.append((cmd, num))
 
-print(len(tails))
+chain = Chain(2)
+for cmd, amount in commands:
+    for i in range(amount):
+        chain.step(cmd)
+
+print(len(chain.tails))
+
+chain = Chain(10)
+for cmd, amount in commands:
+    for i in range(amount):
+        chain.step(cmd)
+
+print(len(chain.tails))
