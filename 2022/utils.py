a2cd95ca2d0d4caafe027acd17e5a826ecc16281
Tom Gooding <tom.pudding@gmail.com>
2022-12-22 23:08:33 +0000

    2d point

--- 2022/utils.py
+++ 2022/utils.py
@@ -92,3 +92,94 @@ class Point3D(object):
 
     def length(self):
         return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+
+@total_ordering
+class Point2D(object):
+    def __init__(self, x: float = 0, y: float = 0):
+        self.x = x
+        self.y = y
+        self.iter_pos = 0
+
+    def __add__(self, other_point):
+        return Point2D(self.x + other_point.x, self.y + other_point.y)
+
+    def __sub__(self, other_point: Point2D):
+        return Point2D(self.x - other_point.x, self.y - other_point.y)
+
+    def __mul__(self, other_point):
+        if isinstance(other_point, Point2D):
+            return Point2D(self.x * other_point.x, self.y * other_point.y)
+        else:
+            return Point2D(self.x * other_point, self.y * other_point)
+
+    def __floordiv__(self, factor):
+        if isinstance(factor, Point2D):
+            return Point2D(self.x // factor.x, self.y // factor.y)
+        else:
+            return Point2D(self.x // factor, self.y // factor)
+
+    def __truediv__(self, factor):
+        if isinstance(factor, Point2D):
+            return Point2D(self.x / factor.x, self.y / factor.y)
+        else:
+            return Point2D(self.x / factor, self.y / factor)
+
+    def __getitem__(self, index):
+        return (self.x, self.y)[index]
+
+    def __setitem__(self, index: int, value: float):
+        setattr(self, ("x", "y")[index], value)
+
+    def __iter__(self)
+        return self
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "(%.2f,%.2f)" % (self.x, self.y)
+
+    def __lt__(self, other):
+        try:
+            if other.x < self.x:
+                return True
+            return other.y < self.y
+        except AttributeError:
+            raise TypeError(f"'<' not supported between instances of {type(self)} and {type(other)}")
+
+    def __eq__(self, other):
+        if not isinstance(other, Point2D):
+            return False
+        try:
+            return self.x == other.x and self.y == other.y
+        except AttributeError:
+            return False
+
+    def __hash__(self):
+        return int(self.x) << 16 | int(self.y)
+
+    def to_float(self):
+        return Point2D(float(self.x), float(self.y))
+
+    def to_int(self):
+        return Point2D(int(self.x), int(self.y))
+
+    def __next__(self):
+        try:
+            out = (self.x, self.y)[self.iter_pos]
+            self.iter_pos += 1
+        except IndexError:
+            self.iter_pos = 0
+            raise StopIteration
+        return out
+
+    def length(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def distance_heuristic(self, other):
+        diff = other - self
+        return (abs(diff.x) + abs(diff.y)) * 20
+
+    def diaglength(self):
+        return max(abs(self.x), abs(self.y))
