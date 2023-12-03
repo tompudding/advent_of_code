33ceb8ef979b67be98fe0c5fdf9903e72a1f7de4
Tom Gooding <tom.pudding@gmail.com>
2023-12-03 10:34:03 +0000

    small optimisation

--- 2023/puzzle_3.py
+++ 2023/puzzle_3.py
@@ -4,9 +4,8 @@ import sys
 # Make the number a class so it can be hashed in a set by its unique id rather than its value, otherwise we
 # might confuse numbers with different positions but the same value
 class Number:
-    def __init__(self, pos, string):
-        self.value = int(string)
-        self.pos = pos
+    def __init__(self, num):
+        self.value = num
 
 
 symbols = {}
@@ -23,7 +22,7 @@ with open(sys.argv[1], "r") as file:
                 for next_pos in range(pos + 1, len(line) + 1):
                     if next_pos == len(line) or not line[next_pos].isdigit():
                         break
-                num = Number(pos, line[pos:next_pos])
+                num = Number(int(line[pos:next_pos]))
                 for i in range(pos, next_pos):
                     numbers[(i, row)] = num
                 pos = next_pos
@@ -31,13 +30,8 @@ with open(sys.argv[1], "r") as file:
                 symbols[(pos, row)] = line[pos]
                 pos += 1
 
-
-symbol_adjacent = set()
-
-for pos in symbols:
-    for x in -1, 0, 1:
-        for y in -1, 0, 1:
-            symbol_adjacent.add((pos[0] + x, pos[1] + y))
+neighbours = [(x, y) for x in (-1, 0, 1) for y in (-1, 0, 1) if (x, y) != (0, 0)]
+symbol_adjacent = {(pos[0] + x, pos[1] + y) for pos in symbols for (x, y) in neighbours}
 
 parts = {numbers[part_pos] for part_pos in symbol_adjacent & numbers.keys()}
 print(sum(part.value for part in parts))
@@ -46,11 +40,7 @@ gear_ratio = 0
 stars = {pos: sym for pos, sym in symbols.items() if sym == "*"}
 
 for pos in stars:
-    adjacent = set()
-    for x in -1, 0, 1:
-        for y in -1, 0, 1:
-            adjacent.add((pos[0] + x, pos[1] + y))
-
+    adjacent = {(pos[0] + x, pos[1] + y) for (x, y) in neighbours}
     adjacent = {numbers[part_pos] for part_pos in adjacent & numbers.keys()}
     if len(adjacent) != 2:
         continue
