8ef73b721beb04651b355615dc7296f4ca829cf3
Tom Gooding <tom.pudding@gmail.com>
2023-12-05 17:30:13 +0000

    Day 5 part 2

--- 2023/puzzle_5.py
+++ 2023/puzzle_5.py
@@ -3,10 +3,22 @@ import bisect
 
 
 class Range:
-    def __init__(self, src, dst, length):
+    def __init__(self, src, length):
         self.src = src
-        self.dst = dst
         self.length = length
+        self.end = self.src + self.length
+
+    def __lt__(self, other):
+        return self.src < other.src
+
+    def __repr__(self):
+        return f"rnge:{self.src} - {self.end}"
+
+
+class RangeMapping(Range):
+    def __init__(self, src, dst, length):
+        super().__init__(src, length)
+        self.dst = dst
         self.adjust = dst - src
 
     def convert(self, input):
@@ -14,9 +26,6 @@ class Range:
             return input
 
         return input + self.adjust
-
-    def __lt__(self, other):
-        return self.src < other.src
 
     def __repr__(self):
         return f"src:{self.src} - {self.src + self.length} dst:{self.dst} - {self.dst + self.length}"
@@ -27,8 +36,8 @@ class Point(Range):
         self.src = num
 
 
-class Mapping:
-    def __init__(self, src_name, dst_name):
+class Ranges:
+    def __init__(self, src_name=None, dst_name=None):
         self.src_name = src_name
         self.dst_name = dst_name
 
@@ -38,6 +47,9 @@ class Mapping:
     def add_range(self, new_range):
         bisect.insort_left(self.ranges, new_range)
 
+
+class Mapping(Ranges):
+    # A mapping is a set of ranges that can convert inputs
     def convert(self, input):
         point = Point(input)
         pos = bisect.bisect_right(self.ranges, point)
@@ -48,6 +60,63 @@ class Mapping:
         match = self.ranges[pos]
 
         return match.convert(input)
+
+    def convert_range(self, input):
+        output_ranges = Ranges()
+
+        pos = 0
+
+        to_convert = Range(input.src, input.length)
+
+        while pos < len(self.ranges):
+            if to_convert.src < self.ranges[pos].src:
+                # Cut off the start
+                cut_off_length = min(self.ranges[pos].src - to_convert.src, to_convert.length)
+
+                output_ranges.add_range(Range(to_convert.src, cut_off_length))
+                if cut_off_length == to_convert.length:
+                    return output_ranges
+
+                to_convert.src = self.ranges[pos].src
+                to_convert.length -= cut_off_length
+                continue
+            elif to_convert.src >= self.ranges[pos].end:
+                pos += 1
+                continue
+            elif to_convert.src >= self.ranges[pos].src and to_convert.src < self.ranges[pos].end:
+                # There's an overlap, so do the conversion
+                start = self.ranges[pos].convert(to_convert.src)
+                # this is an inclusive end
+                last = self.ranges[pos].convert(min(to_convert.end - 1, self.ranges[pos].end - 1))
+                length = last - start + 1
+
+                output_ranges.add_range(Range(start, length))
+                to_convert.src += length
+                if to_convert.length <= length:
+                    return output_ranges
+                to_convert.length -= length
+                continue
+            else:
+                raise jim
+
+        # if there's anything left in the input it should get added as is
+        if to_convert.length > 0:
+            output_ranges.add_range(to_convert)
+
+        return output_ranges
+
+
+class SeedRange(Ranges):
+    def __init__(self):
+        super().__init__("seed", "seed")
+
+    def __repr__(self):
+        out = [f"{len(self.ranges)} ranges:"]
+
+        for rnge in self.ranges:
+            out.append(f"{rnge.src} -> {rnge.end}")
+
+        return "\n".join(out)
 
 
 with open(sys.argv[1], "r") as file:
@@ -70,6 +139,18 @@ def apply_mappings(seed):
     return current
 
 
+def apply_mappings_to_ranges(seed_ranges, mappings):
+    for mapping in mappings:
+        new_ranges = SeedRange()
+        for rnge in seed_ranges.ranges:
+            for new_range in mapping.convert_range(rnge).ranges:
+                new_ranges.add_range(new_range)
+
+        seed_ranges = new_ranges
+
+    return seed_ranges
+
+
 for line in lines[1:]:
     line = line.strip()
     if not line:
@@ -85,7 +166,7 @@ for line in lines[1:]:
 
     dst, src, length = (int(part) for part in line.split())
 
-    current_mapping.add_range(Range(src, dst, length))
+    current_mapping.add_range(RangeMapping(src, dst, length))
 
 if current_mapping:
     mappings.append(current_mapping)
@@ -95,6 +176,20 @@ for seed in seeds:
     location = apply_mappings(seed)
     if smallest is None or location < smallest:
         smallest = location
-    print(seed, location)
+
+
+print(smallest)
+
+# For part 2 the seeds are ranges
+smallest = None
+for i in range(0, len(seeds), 2):
+    seed_range = SeedRange()
+    seed_range.add_range(Range(seeds[i], seeds[i + 1]))
+
+    location_ranges = apply_mappings_to_ranges(seed_range, mappings)
+
+    if smallest is None or location_ranges.ranges[0].src < smallest:
+        smallest = location_ranges.ranges[0].src
+
 
 print(smallest)
