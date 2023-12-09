402e0af02e1c4355b4b322790c5985cfd37fecdd
Tom Gooding <tom.pudding@gmail.com>
2023-12-09 08:47:40 +0000

    Optimization from reddit. Galaxy brain

--- 2023/puzzle_9.py
+++ 2023/puzzle_9.py
@@ -12,26 +12,10 @@ def get_next_item(sequence):
     return total
 
 
-def get_prev_item(sequence):
-    deltas = []
-    current = sequence
-
-    while len(set(current)) != 1:
-        current = [current[i + 1] - current[i] for i in range(len(current) - 1)]
-        deltas.insert(0, current)
-
-    start = deltas[0][0]
-
-    for delta in deltas[1:]:
-        start = delta[0] - start
-
-    return sequence[0] - start
-
-
 with open(sys.argv[1], "r") as file:
     lines = [line.strip() for line in file]
 
 sequences = [[int(num) for num in line.split()] for line in lines]
 
 print(sum(get_next_item(sequence) for sequence in sequences))
-print(sum(get_prev_item(sequence) for sequence in sequences))
+print(sum(get_next_item(sequence[::-1]) for sequence in sequences))
