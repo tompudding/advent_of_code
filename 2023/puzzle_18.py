b2190f73ea1fc10094afa95cd0a3957ba7137532
Tom Gooding <tom.pudding@gmail.com>
2023-12-18 20:54:17 +0000

    2023 day 18 part 2. I looked up a hint and regret it as it's a trivial application of some named algorithm

--- 2023/puzzle_18.py
+++ 2023/puzzle_18.py
@@ -1,21 +1,16 @@
 import sys
+import itertools
 
 
 class RightWall:
     char = "R"
     is_horizontal = True
 
-    def __init__(self, length, colour):
+    def __init__(self, length):
         self.length = length
-        self.colour = colour
-
-        data = int(colour.strip("()#"), 16)
-        self.real_length = data >> 4
-        print(self.real_length)
 
     def generate(self, x, y):
-        self.end_pos = (x + self.length, y)
-        return {(x + i, y) for i in range(0, self.length)}
+        return (x + self.length, y)
 
 
 class LeftWall(RightWall):
@@ -23,8 +18,7 @@ class LeftWall(RightWall):
     is_horizontal = True
 
     def generate(self, x, y):
-        self.end_pos = (x - self.length, y)
-        return {(x - i, y) for i in range(1, self.length + 1)}
+        return (x - self.length, y)
 
 
 class UpWall(RightWall):
@@ -32,8 +26,7 @@ class UpWall(RightWall):
     is_horizontal = False
 
     def generate(self, x, y):
-        self.end_pos = (x, y - self.length)
-        return {(x, y - i) for i in range(0, self.length)}
+        return (x, y - self.length)
 
 
 class DownWall(UpWall):
@@ -41,8 +34,11 @@ class DownWall(UpWall):
     is_horizontal = False
 
     def generate(self, x, y):
-        self.end_pos = (x, y + self.length)
-        return {(x, y + i) for i in range(1, self.length + 1)}
+        return (x, y + self.length)
+
+
+def shoelace(vertices):
+    return sum([(a[0] * b[1]) - (a[1] * b[0]) for a, b in itertools.pairwise(vertices)]) // 2
 
 
 class Quarry:
@@ -50,77 +46,34 @@ class Quarry:
         pos = (0, 0)
 
         self.walls = walls
-        self.horizontal_walls = set()
-        self.vertical_walls = set()
-        caps = set()
+
+        self.vertices = [pos]
 
         for wall in walls:
-            target = self.horizontal_walls if wall.is_horizontal else self.vertical_walls
-            target |= wall.generate(*pos)
-            pos = wall.end_pos
-            caps.add(pos)
+            pos = wall.generate(*pos)
+            self.vertices.append(pos)
 
-        self.all_walls = self.horizontal_walls | self.vertical_walls | caps
-        self.top_left = min(x for x, y in self.all_walls), min(y for x, y in self.all_walls)
-        self.bottom_right = max(x for x, y in self.all_walls), max(y for x, y in self.all_walls)
-        print(f"{self.top_left=} {self.bottom_right=}")
-
-        x_odd = set()
-        y_odd = set()
-        # Fill in the interior
-
-        for y in range(self.top_left[1], self.bottom_right[1] + 1):
-            parity = 0
-            for x in range(self.top_left[0], self.bottom_right[0] + 1):
-                if (x, y) in self.vertical_walls:
-                    parity += 1
-                    continue
-                if parity & 1:
-                    x_odd.add((x, y))
-
-        for x in range(self.top_left[0], self.bottom_right[0] + 1):
-            parity = 0
-            for y in range(self.top_left[1], self.bottom_right[1] + 1):
-                if (x, y) in self.horizontal_walls:
-                    parity += 1
-                    continue
-                if parity & 1:
-                    y_odd.add((x, y))
-
-        print((3, 2) in x_odd, (3, 2) in y_odd)
-        self.interior = x_odd & y_odd
-
-    def get_capacity(self):
-        return len(self.interior | self.all_walls)
-
-    def __repr__(self):
-        out = []
-
-        for y in range(self.top_left[1], self.bottom_right[1] + 1):
-            row = []
-            for x in range(self.top_left[0], self.bottom_right[0] + 1):
-                char = "."
-
-                if (x, y) in self.all_walls:
-                    char = "#"
-                elif (x, y) in self.interior:
-                    char = "x"
-                row.append(char)
-            out.append("".join(row))
-        return "\n".join(out)
+        self.area = shoelace(self.vertices)
+        self.edge = sum(wall.length for wall in walls)
+        self.capacity = (self.area + 1) - (self.edge // 2) + self.edge
 
 
 wall_lookup = {wall.char: wall for wall in (RightWall, UpWall, DownWall, LeftWall)}
 
 with open(sys.argv[1], "r") as file:
-    walls = []
+    part_one_walls = []
+    part_two_walls = []
     for line in file:
         wall_type, length, colour = line.strip().split()
-        walls.append(wall_lookup[wall_type](int(length), colour))
+        part_one_walls.append(wall_lookup[wall_type](int(length)))
 
-quarry = Quarry(walls)
-print(quarry)
-print(quarry.get_capacity())
+        data = int(colour.strip("()#"), 16)
+        length = data >> 4
+        wall_type = [RightWall, DownWall, LeftWall, UpWall][data & 0xF]
+        part_two_walls.append(wall_type(int(length)))
 
-for wall in walls:
-    print(wall.real_length)
+quarry = Quarry(part_one_walls)
+print(quarry.capacity)
+
+quarry = Quarry(part_two_walls)
+print(quarry.capacity)
