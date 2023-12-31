721438909cb3c9ee01ff27df9a069712f6efeb14
Tom Gooding <tom.pudding@gmail.com>
2023-12-31 18:08:42 +0000

    Remove unused parts

--- 2023/day_11.py
+++ 2023/day_11.py
@@ -1,34 +1,16 @@
 import sys
 
-grid = {}
 galaxies = []
 galaxy_rows = set()
 galaxy_cols = set()
 
-empty_rows = set()
-empty_cols = set()
-
-width = 0
-height = 0
-
 with open(sys.argv[1], "r") as file:
     for y, line in enumerate(file):
-        if y + 1 > height:
-            height = y + 1
         for x, char in enumerate(line.strip()):
-            if x + 1 > width:
-                width = x + 1
-            grid[x, y] = char
-
             if char == "#":
                 galaxies.append((x, y))
                 galaxy_rows.add(y)
                 galaxy_cols.add(x)
-
-empty_rows = set(range(height)) - galaxy_rows
-empty_cols = set(range(width)) - galaxy_cols
-
-total = 0
 
 
 def get_distances(multiplier):
@@ -42,10 +24,10 @@ def get_distances(multiplier):
             distance = 0
 
             for x in range(min(a[0], b[0]) + 1, max(a[0], b[0]) + 1):
-                distance += multiplier if x in empty_cols else 1
+                distance += multiplier if x not in galaxy_cols else 1
 
             for y in range(min(a[1], b[1]) + 1, max(a[1], b[1]) + 1):
-                distance += multiplier if y in empty_rows else 1
+                distance += multiplier if y not in galaxy_rows else 1
 
             total += distance
 
