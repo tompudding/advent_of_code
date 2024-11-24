a84aefa6051f1ceee9b6670b75a12f3fb8a95581
Tom Gooding <tom.pudding@gmail.com>
2024-11-24 15:33:40 +0000

    Day 16 part 2 wow

--- 2019/day_16.py
+++ 2019/day_16.py
@@ -32,13 +32,43 @@ class Sequence:
 
         self.sequence = new_seq
 
+    def mega_transform(self):
+        # A B
+        # C D
+        #
+        # D = Sum(B...)
+        # C = A + D
+
+        # Start at the end and work backwards. The last digit always stays the same
+        for pos in range(len(self.sequence)-2, -1, -1):
+            self.sequence[pos] = (self.sequence[pos+1] + self.sequence[pos]) % 10
+
 
     def __repr__(self):
         return ''.join((str(d) for d in self.sequence))
 
 with open(sys.argv[1], 'r') as file:
-    seq = Sequence(file.read().strip())
+    orig = file.read().strip()
+    seq = Sequence(orig)
+
+# The strick for part 2 is that the base sequence is always just a sum in the second half (as the -1s will get pushed back to the end), so each term is the sum of the terms above it,
+# e.g:
+
+# A | B | C
+# A | B + C | C
+
+# So for any particular phase, the last digit is the same, and then we can build the previous digit from the
+# sum of the one above it and the one to its right. We only care about the numbers backwards from the end
+# until our offset, so shrink the list so we only have to worry about what we need
+
+offset = int(orig[:7])
+x = offset % len(orig)
+extra_copies = (len(orig)*10000 - offset - (len(orig)-x) )//len(orig)
+
+seq = Sequence(orig[x:] + orig*extra_copies)
+print(len(seq.sequence))
 
 for i in range(100):
-    seq.transform()
-print(seq)
+    seq.mega_transform()
+
+print(''.join(str(d) for d in seq.sequence[:8]))
