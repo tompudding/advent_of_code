f3aa51598f2954c4f5349c4dbb5bdbef252d1dd0
Tom Gooding <tom.pudding@gmail.com>
2024-12-01 07:22:31 +0000

    Day 1

---
+++ 2024/day_1.py
@@ -0,0 +1,19 @@
+import sys
+import collections
+
+a = []
+b = []
+with open(sys.argv[1], 'r') as file:
+    for line in file:
+        x, y = (int(v) for v in line.strip().split())
+        a.append(x)
+        b.append(y)
+
+a.sort()
+b.sort()
+
+print(sum(abs(x-y) for (x,y) in zip(a,b)))
+
+common = set(a) & set(b)
+counts = collections.Counter(b)
+print(sum((item*counts[item] for item in a)))
