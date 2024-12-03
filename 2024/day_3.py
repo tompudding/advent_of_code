f24051102cc53154f9eb93c2458ab611cb0d658b
Tom Gooding <tom.pudding@gmail.com>
2024-12-03 10:53:10 +0000

    Use cleverer match case syntax

--- 2024/day_3.py
+++ 2024/day_3.py
@@ -11,15 +11,12 @@ print(sum((int(x)*int(y)) for x,y in re.findall(r'mul\((\d\d?\d?),(\d\d?\d?)\)',
 enabled = True
 total = 0
 for match in re.finditer(rf"(do\(\)|don't\(\)|{mul_expr})",data):
-    matched = match.group(1)
-
-    match matched:
-        case 'do()':
+    match match.groups():
+        case ('do()', *_):
             enabled = True
-        case "don't()":
+        case ("don't()", *_):
             enabled = False
-        case matched if matched.startswith('mul') and enabled:
-            x, y = match.groups()[1:]
+        case (mul, x, y) if mul.startswith('mul') and enabled:
             total += int(x)*int(y)
 
 print(total)
