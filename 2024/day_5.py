a97ed7d5a75fbfa889df233d3205cdce98a0eb93
Tom Gooding <tom.pudding@gmail.com>
2024-12-05 06:29:22 +0000

    day 5 tidy

--- 2024/day_5.py
+++ 2024/day_5.py
@@ -1,18 +1,20 @@
 import sys
 import functools
 
+
 class Rule:
     def __init__(self, a, b):
         self.a = a
         self.b = b
 
     def __repr__(self):
-        return f'{self.a} < {self.b}'
+        return f"{self.a} < {self.b}"
+
 
 class Update:
     def __init__(self, numbers):
         self.numbers = numbers
-        self.positions = {num : i for i,num in enumerate(self.numbers)}
+        self.positions = {num: i for i, num in enumerate(self.numbers)}
 
     def is_valid(self, rules):
         for rule in rules:
@@ -25,41 +27,27 @@ class Update:
         return True
 
     def middle(self):
-        assert len(self.numbers)&1
-        return self.numbers[len(self.numbers)//2]
+        assert len(self.numbers) & 1
+        return self.numbers[len(self.numbers) // 2]
 
-    def sort(self, rules_by_num):
-        def compare(a, b):
-            try:
-                if b in rules_by_num[a]:
-                    return -1
-            except KeyError:
-                pass
-
-            try:
-                if a in rules_by_num[b]:
-                    return 1
-            except KeyError:
-                pass
-            return 0
+    def sort(self, compare):
         self.numbers.sort(key=functools.cmp_to_key(compare))
-
 
 
 rules = []
 updates = []
 
 parsing_rules = True
-with open(sys.argv[1], 'r') as file:
+with open(sys.argv[1], "r") as file:
     for line in file:
         line = line.strip()
         if parsing_rules:
             try:
-                rules.append(Rule(*(int(v) for v in line.split('|'))))
+                rules.append(Rule(*(int(v) for v in line.split("|"))))
             except ValueError:
                 parsing_rules = False
         else:
-            updates.append(Update([int(v) for v in line.split(',')]))
+            updates.append(Update([int(v) for v in line.split(",")]))
 
 rules_by_num = {}
 
@@ -69,6 +57,22 @@ for rule in rules:
     except KeyError:
         rules_by_num[rule.a] = {rule.b}
 
+
+def compare(a, b):
+    try:
+        if b in rules_by_num[a]:
+            return -1
+    except KeyError:
+        pass
+
+    try:
+        if a in rules_by_num[b]:
+            return 1
+    except KeyError:
+        pass
+    return 0
+
+
 valid = []
 not_valid = []
 for update in updates:
@@ -77,6 +81,6 @@ for update in updates:
 print(sum(update.middle() for update in valid))
 
 for update in not_valid:
-    update.sort(rules_by_num)
+    update.sort(compare)
 
 print(sum(update.middle() for update in not_valid))
