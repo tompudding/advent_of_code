1a6dcd68b65f685329be1444c506b143b5a2415e
Tom Gooding <tom.pudding@gmail.com>
2024-12-05 06:26:14 +0000

    day 5 part 2

--- 2024/day_5.py
+++ 2024/day_5.py
@@ -1,4 +1,5 @@
 import sys
+import functools
 
 class Rule:
     def __init__(self, a, b):
@@ -27,6 +28,24 @@ class Update:
         assert len(self.numbers)&1
         return self.numbers[len(self.numbers)//2]
 
+    def sort(self, rules_by_num):
+        def compare(a, b):
+            try:
+                if b in rules_by_num[a]:
+                    return -1
+            except KeyError:
+                pass
+
+            try:
+                if a in rules_by_num[b]:
+                    return 1
+            except KeyError:
+                pass
+            return 0
+        self.numbers.sort(key=functools.cmp_to_key(compare))
+
+
+
 rules = []
 updates = []
 
@@ -42,4 +61,22 @@ with open(sys.argv[1], 'r') as file:
         else:
             updates.append(Update([int(v) for v in line.split(',')]))
 
-print(sum(update.middle() for update in updates if update.is_valid(rules)))
+rules_by_num = {}
+
+for rule in rules:
+    try:
+        rules_by_num[rule.a].add(rule.b)
+    except KeyError:
+        rules_by_num[rule.a] = {rule.b}
+
+valid = []
+not_valid = []
+for update in updates:
+    (valid if update.is_valid(rules) else not_valid).append(update)
+
+print(sum(update.middle() for update in valid))
+
+for update in not_valid:
+    update.sort(rules_by_num)
+
+print(sum(update.middle() for update in not_valid))
