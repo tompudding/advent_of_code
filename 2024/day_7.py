c1041b67478be21c159e8402458e9362e87fd34f
Tom Gooding <tom.pudding@gmail.com>
2024-12-08 12:00:28 +0000

    It's much faster to go backwards

--- 2024/day_7.py
+++ 2024/day_7.py
@@ -1,26 +1,44 @@
 import sys
 
 
-def add(a, b):
-    return a + b
+class InputError(Exception):
+    pass
 
 
-def mul(a, b):
-    return a * b
+def div(a, b):
+    if a % b != 0:
+        raise InputError()
+    return a // b
 
 
-def concatenate(a, b):
-    return int(str(a) + str(b))
+def sub(a, b):
+    return a - b
+
+
+def split(a, b):
+    if a == b:
+        raise InputError
+
+    while b:
+        if ((a - b) % 10) != 0:
+            raise InputError
+        a //= 10
+        b //= 10
+
+    return a
 
 
 def possible(result, total, numbers, num, operators):
-    if total > result:
+    if total < result:
         return
     if len(numbers) == num:
         return total == result
     for operator in operators:
-        if possible(result, operator(total, numbers[num]), numbers, num + 1, operators):
-            return True
+        try:
+            if possible(result, operator(total, numbers[num]), numbers, num + 1, operators):
+                return True
+        except InputError:
+            continue
 
 
 class Equation:
@@ -28,6 +46,9 @@ class Equation:
         self.result, rest = line.split(":")
         self.result = int(self.result)
         self.numbers = [int(v) for v in rest.strip().split()]
+
+        # Actually let's go backwards!
+        self.result, self.numbers = self.numbers[0], [self.result] + list(reversed(self.numbers))[:-1]
 
     def possible(self, operators):
         return possible(self.result, self.numbers[0], self.numbers, 1, operators)
@@ -39,14 +60,14 @@ with open(sys.argv[1], "r") as file:
 valid = []
 not_valid = []
 for equation in equations:
-    (valid if equation.possible((add, mul)) else not_valid).append(equation)
+    (valid if equation.possible((div, sub)) else not_valid).append(equation)
 
-part_one = sum(equation.result for equation in valid)
+part_one = sum(equation.numbers[0] for equation in valid)
 
 print(part_one)
 
 part_two = part_one + sum(
-    equation.result for equation in not_valid if equation.possible((add, mul, concatenate))
+    equation.numbers[0] for equation in not_valid if equation.possible((div, sub, split))
 )
 
 print(part_two)
