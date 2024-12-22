e1018da0981647d4a78a0aeffc1d1a1f03f39720
Tom Gooding <tom.pudding@gmail.com>
2024-12-22 07:18:02 +0000

    Day 22 the dummy way

---
+++ 2024/day_22.py
@@ -0,0 +1,56 @@
+import sys
+from collections import defaultdict
+import itertools
+
+
+def op(n):
+    n = (n ^ (n * 64)) & 0xFFFFFF
+    n = (n ^ (n // 32)) & 0xFFFFFF
+    n = (n ^ (n * 2048)) & 0xFFFFFF
+    return n
+
+
+def do_op(n, m):
+    for i in range(m):
+        n = op(n)
+    return n
+
+
+def op_sequence(start, num_required):
+    yield start % 10
+    num = start
+    for i in range(num_required):
+        num = op(num)
+        yield num % 10
+
+
+def get_deltas(seq, num):
+    for i in range(2000 - num):
+        deltas = []
+        yield tuple([(b) - (a) for a, b in itertools.pairwise(seq[i : i + num + 1])]), seq[i + num]
+
+
+with open(sys.argv[1], "r") as file:
+    numbers = [int(line) for line in file]
+
+print(sum(do_op(num, 2000) for num in numbers))
+
+winnings = defaultdict(list)
+
+
+# For part two can we store the sell price of each 4 difference sequence?
+for i, number in enumerate(numbers):
+    num_winnings = {}
+    for deltas, price in get_deltas(tuple(op_sequence(number, 2000)), 4):
+        if deltas in num_winnings:
+            continue
+        num_winnings[deltas] = price
+
+    for deltas, price in num_winnings.items():
+        winnings[deltas].append(price)
+
+winnings = [(deltas, sum(prices), prices) for deltas, prices, in winnings.items()]
+winnings.sort(key=lambda x: x[1])
+
+
+print(winnings[-1][1])
