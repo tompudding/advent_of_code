25f0a15b73601d77b2e9eb5c1e945b24f4dddb19
Tom Gooding <tom.pudding@gmail.com>
2025-12-02 20:47:06 +0000

    Use a less demented algorithm

--- 2025/day_2.py
+++ 2025/day_2.py
@@ -11,38 +11,51 @@ def divisors(n):
     return out
 
 
-def chunks(iterable, length):
-    for i in range(0, len(iterable), length):
-        yield iterable[i : i + length]
-
-
 class Range:
-    def __init__(self, string):
-        self.a, self.b = (int(x.strip()) for x in string.split("-"))
+    def __init__(self, start, end):
+        self.a_str, self.b_str = start, end
+        self.a = int(self.a_str)
+        self.b = int(self.b_str)
+
+        assert len(self.a_str) == len(self.b_str)
 
     def count(self, halves_only):
         count = set()
 
-        for n in range(self.a, self.b + 1):
-            str_n = str(n)
+        # The lengths of our strings are the same by construction.
+        if halves_only:
+            substr_lengths = (len(self.a_str) // 2,)
+            if len(self.a_str) & 1:
+                return 0
+        else:
+            substr_lengths = divisors(len(self.a_str))
 
-            if halves_only:
-                if len(str_n) & 1:
-                    continue
-                substr_lengths = (len(str_n) // 2,)
-            else:
-                substr_lengths = divisors(len(str_n))
+        for length in substr_lengths:
 
-            for length in substr_lengths:
-                if len(set(chunks(str_n, length))) == 1:
-                    count.add(n)
+            lower = int(self.a_str[:length])
+            upper = int(self.b_str[:length])
+            repeats = len(self.a_str) // length
+
+            for n in range(lower, upper + 1):
+                target = int(str(n) * repeats)
+                if target >= self.a and target <= self.b:
+                    count.add(target)
 
         return sum(count)
 
 
+ranges = []
 with open(sys.argv[1]) as file:
     for line in file:
-        ranges = [Range(part) for part in line.split(",")]
+        for num_range in line.split(","):
+            start, end = (val.strip() for val in num_range.split("-"))
+
+            if len(start) == len(end):
+                ranges.append(Range(start, end))
+            else:
+                # When they're not the same length split it into two regions for my sanity
+                ranges.append(Range(start, "9" * len(start)))
+                ranges.append(Range("1" + "0" * len(start), end))
         break
 
 print(sum(r.count(halves_only=True) for r in ranges))
